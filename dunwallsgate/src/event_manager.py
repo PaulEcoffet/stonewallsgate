8f6d433d01690abf43327653d65447ac2dfedc7d
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-11-09 16:08:54 +0100

    debut of refactoring for eventmanager

--- dunwallsgate/src/event_manager.py
+++ dunwallsgate/src/event_manager.py
@@ -11,10 +11,22 @@ class EventManager():
     """
 
     def __init__(self):
-        self.globalCallbacks = {"mouse": {}, "key": {}, "custom": {}}
-        self.screenCallbacks = {"mouse": {}, "key": {}, "custom": {}}
+        self.callbacks = {}
 
-    def on_key_down(self, callback, which=None, global_=False):
+    def purge_callbacks(self, category):
+        try:
+            self.callbacks[category] = {}
+        except KeyError:
+            pass
+
+    def get_category(self, cat):
+        try:
+            self.callbacks[cat]
+        except KeyError:
+            self.callbacks[cat] = {}
+        return self.callbacks[cat]
+
+    def on_key_down(self, callback, which=None, cat="screen"):
         """
         Register an event when a key is pressed
         callback - The function to call when the event occurs
@@ -24,16 +36,13 @@ class EventManager():
         global_ - specify if the callbacks are automatically deactivate when
                   the screen change or not
         """
-        if global_:
-            callbacks = self.globalCallbacks
-        else:
-            callbacks = self.screenCallbacks
+        callbacks = self.get_category(cat)
         try:
             callbacks["key"].append((which, callback))
         except:
             callbacks["key"] = [(which, callback)]
 
-    def on_click_on(self, collidable, callback, button=1, global_=False):
+    def on_click_on(self, collidable, callback, button=1, cat="screen"):
         """
         Register a callback called when the collidable object is clicked.
         collidable - The object that might be clicked, must implement a
@@ -44,10 +53,7 @@ class EventManager():
                  ex : 1 - left mouse button, (1, 2) - left and right mouse
                  buttons, 0 : all mouse buttons (including scroll up & down)
         """
-        if global_:
-            callbacks = self.globalCallbacks
-        else:
-            callbacks = self.screenCallbacks
+        callbacks = self.get_category(cat)
         if not isinstance(button, tuple):
             button = (button,)
         try:
@@ -55,11 +61,8 @@ class EventManager():
         except:
             callbacks["mouse"] = [(button, collidable, callback)]
 
-    def on_quit(self, callback, global_=False):
-        if global_:
-            callbacks = self.globalCallbacks
-        else:
-            callbacks = self.screenCallbacks
+    def on_quit(self, callback, cat="screen"):
+        callbacks = self.get_category(cat)
         try:
             callbacks["quit"].append(callback)
         except KeyError:
@@ -69,15 +72,15 @@ class EventManager():
     def run(self, events):
         for event in events:
             if event.type == pg.KEYDOWN:
-                for callback in self.both_callbacks("key"):
+                for callback in self.all_callbacks("key"):
                     if callback[0] is None or callback[0] == event.key or\
                             callback[0] == event.unicode:
                         callback[1](event.key, event.unicode)
             elif event.type == pg.QUIT:
-                for callback in self.both_callbacks("quit"):
+                for callback in self.all_callbacks("quit"):
                     callback()
             elif event.type == pg.MOUSEBUTTONUP:
-                for callback in self.both_callbacks("mouse"):
+                for callback in self.all_callbacks("mouse"):
                     if event.button in callback[0]:
                         try:
                             if callback[1].collidepoint(event.pos):
@@ -85,17 +88,14 @@ class EventManager():
                         except AttributeError:
                             try:
                                 if (callback[1].get_rect()
-                                    .collidepoint(event.pos)):
+                                        .collidepoint(event.pos)):
                                     callback[2]()
                             except AttributeError:
                                 if callback[1].rect.collidepoint(event.pos):
                                     callback[2]()
 
-    def on_custom_event(self, event_name, data, callback, global_=False):
-        if global_:
-            callbacks = self.globalCallbacks
-        else:
-            callbacks = self.screenCallbacks
+    def on_custom_event(self, event_name, data, callback, cat="screen"):
+        callbacks = self.get_category(cat)
         try:
             callbacks["custom"][event_name].append(callback)
         except KeyError:
@@ -103,22 +103,18 @@ class EventManager():
         return (callbacks["custom"][event_name], callback)  # ID used to remove
                                                             # event
 
-    def both_callbacks(self, *keys):
-        stop_gcall = False
-        stop_scall = False
-        gcall = self.globalCallbacks
-        scall = self.screenCallbacks
-        for key in keys:
-            if not stop_gcall:
-                try:
-                    gcall = gcall[key]
-                except KeyError:
-                    stop_gcall = True
-                    gcall = []
-            if not stop_scall:
-                try:
-                    scall = scall[key]
-                except KeyError:
-                    stop_scall = True
-                    scall = []
-        return itertools.chain(gcall, scall)
+    def all_callbacks(self, key):
+        for category in self.callbacks.keys():
+            callbacks[category] = self.callbacks[key]
+        for cat, cat_callbacks in callback.items():
+            try:
+                cat = gcall[key]
+            except KeyError:
+                stop_gcall = True
+                gcall = []
+            try:
+                scall = scall[key]
+            except KeyError:
+                stop_scall = True
+                scall = []
+        return itertools.chain(callbacks)
