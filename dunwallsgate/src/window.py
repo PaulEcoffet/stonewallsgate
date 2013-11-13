2403557d1266f14eeff443ba5be74da810ad0431
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-11-13 12:15:03 +0100

    Update window.py

--- dunwallsgate/src/window.py
+++ dunwallsgate/src/window.py
@@ -4,12 +4,13 @@ import pygame
 import pygame.locals as pg
 
 from event_manager import EventManager
+from screens.homescreen import HomeScreen
 
 FPS = 30
 
 
 class Window():
-    def __init__(self, screen):
+    def __init__(self,):
         """
         Define a new window with a given screen
 
@@ -19,7 +20,7 @@ class Window():
         pygame.display.set_caption("Dunwall's Gate")
         self.eventmanager = EventManager()
         self._screen = None
-        self.set_screen(screen)
+        self.set_screen(HomeScreen())
         self.fpsClock = pygame.time.Clock()
         self.do_run = True
 
@@ -28,10 +29,9 @@ class Window():
         Define the screen to be displayed in the window
         """
         if self._screen is not None:
-            self._screen.shutdown()   
+            self._screen.shutdown()
         self._screen = screen
         self._screen.start(self, self.eventmanager)
-
 
     def set_do_run(self, value=True):
         self.do_run = value
@@ -40,7 +40,7 @@ class Window():
         """
         Run the game
         """
-        self.eventmanager.on_quit(lambda: self.set_do_run(False), True)
+        self.eventmanager.on_quit(lambda x: self.set_do_run(False), "global")
         while self.do_run:
             self._screen.draw()
             self.eventmanager.run(pygame.event.get())
