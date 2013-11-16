e3929e15cd4eb4c030d6aa8b37037895da399e44
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-11-16 17:53:41 +0100

    Addition of some properties for the game object

--- dunwallsgate/src/game.py
+++ dunwallsgate/src/game.py
@@ -1,14 +1,32 @@
 from screens.storyscreen import StoryScreen
 
+
 class Game():
     """Contains the current state of the game"""
-    
+
     def __init__(self):
         self.window = None
-        self.quests = []
-        self.hero = {"current_scene" : "scene1"}
+        self.quests = quest.get_quests_dict()
+        self.base = Base()
+        self.hero = {"current_scene": "scene1"}
         self.screen = StoryScreen(self.hero)
-    
+
     def start(self, window):
         self.window = window
         self.window.set_screen(self.screen)
+
+
+class Base():
+    """
+    Contains the state of the base of the hero.
+    """
+
+    def __init__(self, food=0, water=0, energy=0, inventory=None):
+        self._food = food
+        self._water = water
+        self._energy = energy
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = Inventory()
+        self.characters = []
