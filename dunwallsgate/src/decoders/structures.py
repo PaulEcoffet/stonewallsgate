38a35e2772e53f667cd3d6df1657cd2c4c9e1760
NestarZ <elias.rouzlane@gmail.com>
2013-11-17 08:32:05 +0100

    structures.py debug

--- dunwallsgate/src/decoders/structures.py
+++ dunwallsgate/src/decoders/structures.py
@@ -1,14 +1,10 @@
 #!/bin/python3
 
-class Event():
-    pass
-
 class Scene():
     def __init__(self):
         self.background = ""  # txt (image?)
         self.events = {}  # dct
 
-<<<<<<< HEAD
 ######
         
 class Event():
@@ -28,34 +24,24 @@ class Event():
             self.begin = False
         if len(self.dialogs) != 0:
             return self.dialogs[0]
-=======
->>>>>>> 78c7f4051a12a33dee70e3112714b5bcdaacfb65
+        
+class Conditions():
+    def __init__(self):
+        self.test = ""
+        self.params = []
+        self.param = ""
+        
+class Triggers():
+    def __init__(self):
+        self.trigger = ""
+        self.params = []
+######
 
 class Dialog():
     def __init__(self):
         self.character = ""
-<<<<<<< HEAD
+
         self.message = []
         self.old_messages = []
         self.begin = True
-        
-    """def next(self):
-        if self.begin:
-            self.begin = False
-        else:
-            self.old_messages.append(self.messages[0])
-            print(self.messages[0])
-            del self.messages[0]
 
-        return self.messages[0]
-    
-    def previous(self):
-        if len(self.old_messages) > 0:
-            self.messages.append(self.old_messages[len(self.old_messages)-1])
-            del self.old_messages[len(self.old_messages)-1]
-            return self.next()"""
-
-######
-=======
-        self.message = ""
->>>>>>> 78c7f4051a12a33dee70e3112714b5bcdaacfb65
