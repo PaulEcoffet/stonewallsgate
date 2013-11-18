ed2c485318bf0a0391e33fc5d723ac53487043ef
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-11-18 17:12:25 +0100

    additions of event in events list + dialogues renamed

--- dunwallsgate/src/decoders/json_structures.py
+++ dunwallsgate/src/decoders/json_structures.py
@@ -32,22 +32,23 @@ def get_events(scene_name):
         for json_event in json_events:
             event = Event()
             event.conditions = json_event['conditions']
-            event.dialogs = get_dialog(current_folder, json_event['dialogs'])
+            event.dialogs = get_dialogues(current_folder, json_event['dialogs'])
             event.triggers = json_event['triggers']
+            events.append(event)
     return events
 
 
-def get_dialog(current_folder, ref_dialogs):
+def get_dialogues(current_folder, ref_dialogues):
     """
     Get and decode the right json dialog file to fill
     each parameters in a Dialog object that is returned.
     """
     with open(
-            os.path.join(current_folder, "dialogues", ref_dialogs + ".json"),
+            os.path.join(current_folder, "dialogues", ref_dialogues + ".json"),
             "r", encoding="latin-1") as json_file:
         list_dct = json.load(json_file)
-        dialogs = [Dialog() for dct in list_dct]
+        dialogues = [Dialog() for dct in list_dct]
         for i, dct in enumerate(list_dct):
-            dialogs[i].character = dct['character']
-            dialogs[i].message = dct['message']
-    return dialogs
+            dialogues[i].character = dct['character']
+            dialogues[i].message = dct['message']
+    return dialogues
