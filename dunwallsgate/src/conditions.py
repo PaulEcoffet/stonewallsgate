0f29648abaf6e2879ee5185b78fdeb4a24db0eda
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-07 13:20:46 +0100

    Conditions & triggers now use the correct inventory sys

--- dunwallsgate/src/conditions.py
+++ dunwallsgate/src/conditions.py
@@ -1,5 +1,8 @@
 """
 """
+
+import inventory
+
 INACTIVE = 0
 ACTIVE = 1
 VALIDATED = 2
@@ -9,17 +12,15 @@ def get_conditions_dict(game):
     Return a dict with all the conditions that can be called from a json object
     """
     return {
-            "hero_name": lambda args: game.hero_state["hero_name"],
-            "is_item": lambda item: item in game.hero_state["items"],
-            "is_quest_valid": lambda quest: game.hero_state["quests"][quest] == VALIDATED,
-            "is_quest_active": lambda quest: game.hero_state["quests"][quest] == ACTIVE,
-            "is_quest_inactive": lambda quest: game.hero_state["quests"][quest] == INACTIVE,
-            "combat_state": lambda state: game.combat_state == state,
-            "has_fellow": lambda args: args[0] in game.hero_fellows,
-            "is_food": lambda args: game.hero_state["is_food"],
-            "is_water": lambda args: game.hero_state["is_water"],
-            "is_energy": lambda args: game.hero_state["is_energy"],
-            "water_under": lambda args: game.water < args[0]
-            }
-
-    
+        "hero_name": lambda args: game.hero_state["hero_name"],
+        "is_item": lambda item: inventory.create_item(item) in game.hero.inventory,
+        "is_quest_valid": lambda quest: game.hero_state["quests"][quest] == VALIDATED,
+        "is_quest_active": lambda quest: game.hero_state["quests"][quest] == ACTIVE,
+        "is_quest_inactive": lambda quest: game.hero_state["quests"][quest] == INACTIVE,
+        "combat_state": lambda state: game.combat_state == state,
+        "has_fellow": lambda args: args[0] in game.hero_fellows,
+        "is_food": lambda args: game.hero_state["is_food"],
+        "is_water": lambda args: game.hero_state["is_water"],
+        "is_energy": lambda args: game.hero_state["is_energy"],
+        "water_under": lambda args: game.water < args[0]
+    }
--- dunwallsgate/src/triggers.py
+++ dunwallsgate/src/triggers.py
@@ -11,29 +11,35 @@
 "unlock": game.unlock
 """
 
+import inventory
+
 INACTIVE = 0
 ACTIVE = 1
 VALIDATED = 2
+
 
 def get_triggers_dict(game):
     """
     Return a dict with all the triggers that can be called from a json object
     """
     return {
-            "active_quest": lambda quest: active_quest(game, quest),
-            "valid_quest": lambda quest: valid_quest(game, quest),
-            "add_item": lambda item: game.hero.inventory._items.append(item),
-            "inactive_quest": lambda quest: inactive_quest(game, quest),
-            "switch_scene": lambda name: switch_scene(game, name),
-            "modif_health" : lambda hp: game.hero.health + hp,
-            "def_next_scene": lambda name: def_next_scene(game, name),
-            "restart_event": lambda args: restart_event(game),
-            "force_stop_event": lambda args: force_stop_event(game)
-            }
+        "active_quest": lambda quest: active_quest(game, quest),
+        "valid_quest": lambda quest: valid_quest(game, quest),
+        "add_item": lambda item: game.hero.inventory.add(
+            inventory.create_item(item)),
+        "inactive_quest": lambda quest: inactive_quest(game, quest),
+        "switch_scene": lambda name: switch_scene(game, name),
+        "modif_health": lambda hp: game.hero.health + hp,
+        "def_next_scene": lambda name: def_next_scene(game, name),
+        "restart_event": lambda args: restart_event(game),
+        "force_stop_event": lambda args: force_stop_event(game)
+    }
+
 
 def def_next_scene(game, name):
     """ Utilisé via un choix notamment ou à la fin d'un combat (perdu/gagné) - On force le changement de scène """
     game.next_scene = name
+
 
 def switch_scene(game, name):
     """ Si on forçage de changement de scène est en cours, sinon on met la scène par défaut """
@@ -42,18 +48,23 @@ def switch_scene(game, name):
     else:
          assert isinstance(name, str), "Aucune scène n'a été mise en paramètre ni dans events.json ni dans les dialogues, ni dans les combats (pourtant un switch_scene a été demandé => vérifiez vos JSON)"
          game.change_scene(name)
-         
+
+
 def active_quest(game, quest):
     game.hero_state["quests"][quest] = ACTIVE
-    
+
+
 def valid_quest(game, quest):
     game.hero_state["quests"][quest] = VALIDATED
 
+
 def inactive_quest(game, quest):
     game.hero_state["quests"][quest] = INACTIVE
-    
+
+
 def restart_event(game):
     game.restart_event = True
+
 
 def force_stop_event(game):
     game.game_event.event_done = True
