5b04392dacf920989c5e5633f9d813d23c83ad36
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-08 19:02:14 +0100

    Characters can now attack

--- dunwallsgate/src/screens/battlescreen.py
+++ dunwallsgate/src/screens/battlescreen.py
@@ -4,6 +4,8 @@ import pygame
 
 from customsprites import Portrait, LifeBar
 from button import Button
+from screens.text_render import TextRender
+
 
 class BattleScreen():
     """
@@ -27,10 +29,10 @@ class BattleScreen():
         self.eventmanager = None
         self.game = game
         self.portraits = []
-        self.highlighted_pending = []
-        self.portrait_update = False
+        self.highlighted = []
         self.buttons = None
-        self.old_portraits_elements = None
+        self.portraits_elements = None
+        self.atk_cat = object()
 
     def start(self, window, eventmanager):
         self.window = window
@@ -49,35 +51,25 @@ class BattleScreen():
 
         self.graphic_elements = pygame.sprite.OrderedUpdates(self.combat_box,
                                                              self.info_box)
-        self.lifebars_elements = pygame.sprite.OrderedUpdates()
-        self.portraits_elements = pygame.sprite.RenderUpdates()
+        self.lifebars_elements = pygame.sprite.OrderedUpdates(
+            [lifebar for lifebar in self.lifebars.values()])
+        self.portraits_elements = pygame.sprite.RenderUpdates([portrait for portrait in self.portraits.values()])
 
     def update(self):
-            self.lifebars_elements.update()
-            self.buttons.update()
-            if self.portrait_update or self.start_battle:
-                self.old_portraits_element = self.portraits_elements 
-                self.portraits_elements = pygame.sprite.RenderUpdates(
-                    self.new_portraits)
-                self.portraits_elements.update(self.highlighted_pending)
-                self.highlighted_pending = []
+        self.lifebars_elements.update()
+        self.buttons.update()
+        self.portraits_elements.update(self.highlighted)
 
     def draw(self):
         if self.start_battle:
             self.surface.blit(self.background, (0, 0))
             self.start_battle = False
         else:
-            if self.portrait_update:
-                self.old_portraits_elements.clear(self.surface, self.background)
             self.lifebars_elements.clear(self.surface, self.background)
-            self.lifebars_elements = pygame.sprite.OrderedUpdates(
-                [lifebar for lifebar in self.lifebars.values()])
             self.lifebars_elements.draw(self.surface)
+            self.portraits_elements.clear(self.surface, self.background)
             self.portraits_elements.draw(self.surface)
-
             self.graphic_elements.clear(self.surface, self.background)
-            self.graphic_elements = pygame.sprite.OrderedUpdates(
-                self.combat_box, self.info_box)
             self.graphic_elements.draw(self.surface)
             self.buttons.clear(self.surface, self.background)
             self.buttons.draw(self.surface)
@@ -101,6 +93,7 @@ class BattleScreen():
             self.purge_box(self.info_box, 195)
         if not self.attack_btn:
             self.attack_btn = Button(self.eventmanager, self, "ATTACK")
+            self.attack_btn.on_click(lambda e: self.show_attack_action())
         if not self.switchwep_btn:
             self.switchwep_btn = Button(self.eventmanager, self, "WEAPONS")
         if not self.run_btn:
@@ -110,36 +103,54 @@ class BattleScreen():
         self.buttons = pygame.sprite.RenderUpdates(self.attack_btn, self.switchwep_btn,
                                                    self.run_btn, self.bag_btn)
 
+    def show_attack_action(self):
+        text = TextRender((self.info_box.image.get_width() - 20,
+                           self.info_box.image.get_height() - 20),
+                          "larabiefont", 25,
+                          (255, 158, 0), "Choisissez votre cible")
+        portrait_targets = [(char, self.portraits[char])
+                            for char in
+                            self.battle.possible_targets_attack()]
+        for char, portrait in portrait_targets:
+            self.eventmanager.on_click_on(portrait, lambda e: self.do_attack(char), self.atk_cat)
+        self.highlighted = [portrait for char, portrait in portrait_targets]
+        self.info_box.image.blit(text.next(), (10, 10))
+
+    def do_attack(self, char):
+        self.battle.do_attack(char)
+        self.end_turn()
+
+    def end_turn(self):
+        self.battle.end_turn()
+        self.highlighted = [self.portraits[self.battle.playing_char]]
+        self.purge_box(self.info_box)
+
     def purge_box(self, sprite, alphakey=140):
         sprite.image.fill((0, 0, 0, alphakey))
 
     def init_battle(self):
-        self.characs = []
         self.lifebars = {character: LifeBar(character) for character
                          in self.battle.all_characters}
-        self.set_elements(self.battle.team1, "allies")
-        self.set_elements(self.battle.team2, "ennemies")
-        self.new_portraits = self.characs
+        self.portraits = {character: Portrait(self.game.cache, character) for character
+                          in self.battle.all_characters}
+        self.place_elements()
+        self.highlighted = [self.portraits[self.battle.playing_char]]
 
-    def set_elements(self, characters, type_):
-        if type_ == "allies":
-            position_portraits = lambda i: (190 * (i * 1.2 + 1), 217)
-            position_bars = lambda i: (160 * (i * 1.4 + 1), 365)
-        else:
-            position_portraits = lambda i: (100 * (i * 1.2 + 1) + 775, 10)
-            position_bars = lambda i: (100 * (i * 1.4 + 1) + 740, 160)
-        for i, companion in enumerate(characters):
-            print(companion)
-            portrait = self.get_character_portrait(companion, "front")
+    def place_elements(self):
+        i_team1 = -1  # Correct padding
+        i_team2 = -1  # Correct padding
+        i = -1
+        for character, portrait in self.portraits.items():
+            if character in self.battle.team1:
+                position_portraits = lambda i: (190 * (i * 1.2 + 1), 217)
+                position_bars = lambda i: (160 * (i * 1.4 + 1), 365)
+                i_team1 += 1
+                i = i_team1
+            else:
+                position_portraits = lambda i: (100 * (i * 1.2 + 1) + 775, 10)
+                position_bars = lambda i: (100 * (i * 1.4 + 1) + 740, 160)
+                i_team2 += 1
+                i = i_team2
             portrait.resize(300, 150)
             portrait.move(*position_portraits(i))
-            self.characs.append(portrait)
-            self.lifebars[companion].move(*position_bars(i))
-            
-    def get_character_portrait(self, charac, cat):
-        for portrait in self.portraits:
-            if (charac, cat) == portrait.id:
-                return portrait
-        self.portraits.append(
-            Portrait(self.game.cache, charac, cat))
-        return self.portraits[-1]
+            self.lifebars[character].move(*position_bars(i))
