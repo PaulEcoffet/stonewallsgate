7ef696337dc4481278ac1fbfdeb230f96556c4c0
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-09 16:22:28 +0100

    Cleaning up

--- dunwallsgate/src/screens/storyscreen.py
+++ dunwallsgate/src/screens/storyscreen.py
@@ -3,9 +3,7 @@
 import pygame
 import pygame.locals as pg
 
-from screens.text_render import TextRender
 from customsprites import Portrait
-from character import Character
 from button import Button
 
 
@@ -47,7 +45,7 @@ class StoryScreen():
         self.eventmanager.on_key_down(self.show_dialogue,
                                       self.next_dialogue_cat, pg.K_SPACE)
         self.eventmanager.on_click_on(self.suite_box, self.show_dialogue,
-                                      self.next_dialogue_cat)                           
+                                      self.next_dialogue_cat)
         self.init_story()
 
     def smooth_update(self, event):
@@ -57,13 +55,13 @@ class StoryScreen():
             self.set_background(self.event.background)
             self.new_background = True
         self.init_story()
-        
+
     def update(self):
         if self.choices:
             self.suite_box.click_time = 0
         self.graphic_elements.update()
         if self.portrait_update or self.start_scene:
-            self.old_portraits_element = self.portraits_elements 
+            self.old_portraits_element = self.portraits_elements
             self.portraits_elements = pygame.sprite.RenderUpdates(
                 self.new_portraits)
             self.portraits_elements.update(self.highlighted_pending)
@@ -84,7 +82,7 @@ class StoryScreen():
         self.graphic_elements.draw(self.surface)
         if self.portrait_update or self.start_scene:
             self.old_portraits_element.clear(self.surface,
-                                          self.background)
+                                             self.background)
             self.portraits_elements.draw(self.surface)
             self.portrait_update = False
 
@@ -99,19 +97,21 @@ class StoryScreen():
             self.purge_textbox()
 
         if not self.suite_box:
-            self.suite_box = Button(self.eventmanager, self, "SUITE", (77, 20),"box_options")
+            self.suite_box = Button(self.eventmanager,
+                                    self, "SUITE", (77, 20), "box_options")
 
     def purge_textbox(self):
         self.dialogue_box.image.fill((0, 0, 0, 140))
-        
+
     def set_background(self, background):
         self.bg_ref = background
         try:
             self.background = self.game.cache.image_backgrounds[self.bg_ref]
         except KeyError:
             self.background = self.game.cache.image_backgrounds["default"]
-            print("Warning: images/scenes/%s.png does not exist ! Default BG has been set"%self.bg_ref)
-            
+            print(("Warning: images/scenes/%s.png does not exist !"
+                  "Default BG has been set") % self.bg_ref)
+
     def init_story(self):
         self.game.cache.clear_dialogues()
         self.game.cache.format_dialogues(self.event.dialogues)
@@ -144,22 +144,21 @@ class StoryScreen():
         for _id in self.msg:
             if self.msg[_id] and _id in ["talker", "hearer"]:
                 charac = self.game.get_character(self.msg[_id])
-                portrait = self.get_character_portrait(charac, 
-                    "front")
+                portrait = self.get_character_portrait(charac, "front")
                 if _id == "talker":
                     self.highlighted_pending.append(portrait)
                     if self.left_one != self.msg["hearer"]:
                         self.left_one = self.msg[_id]
                 if self.msg[_id] == self.left_one:
                     portrait.rect = (portrait.image
-                                        .get_rect(midtop=(250, 160)))
+                                     .get_rect(midtop=(250, 160)))
                 else:
                     portrait.rect = (portrait.image
-                                        .get_rect(midtop=(800, 160)))
+                                     .get_rect(midtop=(800, 160)))
                 portrait.resize()
                 portraits.append(portrait)
         return portraits
-    
+
     def get_character_portrait(self, charac, cat):
         for portrait in self.portraits:
             if (charac, cat) == portrait.id:
@@ -167,7 +166,7 @@ class StoryScreen():
         self.portraits.append(
             Portrait(self.game.cache, charac, cat))
         return self.portraits[-1]
-    
+
     def ask_choices(self):
         choices_sprite = []
         if self.msg["choices"]:
@@ -175,49 +174,52 @@ class StoryScreen():
             for i, choice in enumerate(self.msg["choices"]):
                 if len(self.msg["choices"]) > 3:
                     choices_sprite.append(
-                        Button(self.eventmanager, self,  choice["txt"], 
-                            (435, 27),"dialogue_choices"))
+                        Button(self.eventmanager, self,  choice["txt"],
+                               (435, 27), "dialogue_choices"))
                     if len(choices_sprite) > 3:
-                        choices_sprite[-1].rect = (choices_sprite[i].image
-                                                   .get_rect(x=530, y=35 *i + 305))
+                        choices_sprite[-1].rect = \
+                            (choices_sprite[i].image.get_rect(x=530,
+                                                              y=35 * i + 305))
                     else:
-                        choices_sprite[-1].rect = (choices_sprite[i].image
-                                                   .get_rect(x=75, y=35 * i + 425))
+                        choices_sprite[-1].rect = \
+                            (choices_sprite[i].image.get_rect(x=75,
+                                                              y=35 * i + 425))
                 else:
                     choices_sprite.append(
-                        Button(self.eventmanager, self,  choice["txt"], 
-                            (870, 27),"dialogue_choices"))
+                        Button(self.eventmanager, self,  choice["txt"],
+                               (870, 27), "dialogue_choices"))
                     choices_sprite[-1].rect = (choices_sprite[i].image
-                                               .get_rect(x=75, y=35 * i + 425))
+                                               .get_rect(x=75,
+                                                         y=35 * i + 425))
                 if "triggers" in choice:
                     for trigger in choice["triggers"]:
-                        self.eventmanager.on_click_on(choices_sprite[-1], 
-                            (lambda trigger_cat, params: \
-                                lambda e: self.add_trigger(trigger_cat, params)) \
-                                    (trigger.get("trigger_cat", None), 
-                                        trigger.get("params", None)), 
-                                        self.choice_actions_cat)
+                        self.eventmanager.on_click_on(
+                            choices_sprite[-1], (lambda trigger_cat, params:
+                                lambda e: self.add_trigger(trigger_cat, params))
+                                    (trigger.get("trigger_cat", None),
+                                     trigger.get("params", None)),
+                                     self.choice_actions_cat)
                 elif "trigger_cat" in choice:
-                    self.eventmanager.on_click_on(choices_sprite[-1], 
+                    self.eventmanager.on_click_on(choices_sprite[-1],
                             (lambda trigger_cat, params: \
                                 lambda e: self.add_trigger(trigger_cat, params)) \
-                                    (choice.get("trigger_cat", None), 
-                                        choice.get("params", None)), 
-                                        self.choice_actions_cat)
-                self.eventmanager.on_click_on(choices_sprite[-1], 
-                    lambda e: self.activate_dialogue_events(), 
+                                    (choice.get("trigger_cat", None),
+                                     choice.get("params", None)),
+                                     self.choice_actions_cat)
+                self.eventmanager.on_click_on(choices_sprite[-1],
+                    lambda e: self.activate_dialogue_events(),
                         self.choice_actions_cat)
-                self.eventmanager.on_click_on(choices_sprite[-1], 
+                self.eventmanager.on_click_on(choices_sprite[-1],
                     lambda e: self.purge_choice_actions(),
                         self.choice_actions_cat)
-                self.eventmanager.on_click_on(choices_sprite[-1], 
+                self.eventmanager.on_click_on(choices_sprite[-1],
                     self.show_dialogue, self.choice_actions_cat)
         return choices_sprite
-    
+
     def add_trigger(self, trigger_cat, params=None):
         if trigger_cat:
             self.game.game_event.game_triggers[trigger_cat](params)
-        
+
     def activate_dialogue_events(self):
         self.eventmanager.unlock_categories(self.next_dialogue_cat)
 
