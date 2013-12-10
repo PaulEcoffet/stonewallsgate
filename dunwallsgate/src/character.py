34830be26351a8d6e36566fe41eed257fcc418c1
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-10 14:46:04 +0100

    inventory maintenant chargé

--- dunwallsgate/src/character.py
+++ dunwallsgate/src/character.py
@@ -32,7 +32,10 @@ class Character(object):
         else:
             data = get_character_data("unknown")
         self._health = 0
-        self._inventory = inventory.Inventory()
+        if data["inventory"]:
+            self._inventory = inventory.Inventory(data["inventory"])
+        else:
+            self._inventory = inventory.Inventory()
         self.name = self.operations(custom.get("name", data["name"]))
         self.maxhealth = self.operations(custom.get(
             "maxhealth", data["maxhealth"]))
