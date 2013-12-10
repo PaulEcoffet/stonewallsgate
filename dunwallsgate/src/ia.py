b2e1c7ce42d4a3a5c596fdf4f5471264a0cf2703
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-10 15:54:53 +0100

    Addition of docstring

--- dunwallsgate/src/ia.py
+++ dunwallsgate/src/ia.py
@@ -1,12 +1,24 @@
+"""
+Ce module contient l'IA du jeu. Son comportement est de choisir la meilleure
+arme qu'elle peut avoir, puis d'attaquer avec une personne aléatoire.
+Celle-ci pourrait attaquer de manière systématique le plus faible, mais cela
+pourrait rendre le jeu trop difficile et prévisible pour le joueur.
+"""
 import random
 
 
 class IA(object):
+    """
+    Représente une IA.
+    """
 
     def __init__(self, battle):
         self.battle = battle
 
     def play(self):
+        """
+        Choisit la meilleure arme et attaque avec une fois équipé.
+        """
         playing_char = self.battle.playing_char
         best = self.get_best_weapon()
         if (best[0] is not playing_char.weapon
@@ -17,6 +29,9 @@ class IA(object):
                 random.choice(self.battle.possible_targets_attack()))
 
     def get_best_weapon(self):
+        """
+        Trouve la meilleure arme présente dans l'inventaire de l'IA
+        """
         playing_char = self.battle.playing_char
         best = (playing_char.inventory.get_first("bare_hands"), None)
         for weapon in playing_char.inventory.weapons:
