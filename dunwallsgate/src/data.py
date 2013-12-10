40f6cfb3d4b786e6703fa232b606f65d1701fe0d
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-10 16:02:01 +0100

    Addition of docstrings

--- dunwallsgate/src/data.py
+++ dunwallsgate/src/data.py
@@ -1,33 +1,45 @@
+"""
+Ce module permet d'obtenir le chemin absolu des différents fichiers de données.
+Ainsi, le fichier peut être exécuter depuis n'importe quel "working directory"
+sans qu'il y ait de problème de chemin.
+"""
 import os.path
 import sys
 
 
 def _module_path():
+    """Obtient le chemin absolu du module actuel, même si le fichier est
+    "frozen", c'est-à-dire mis dans un exécutable."""
     if hasattr(sys, "frozen"):
         return os.path.dirname(sys.executable)
     return os.path.dirname(__file__)
 
 
 def get_data_path():
+    """Donne le chemin du dossier data comlet"
     return os.path.abspath(
         os.path.join(_module_path(), '../data/'))
 
 
 def get_image_path(path=""):
+    """Donne le chemin absolu du data/images"""
     return os.path.abspath(
         os.path.join(get_data_path(), "images/", path))
 
 
 def get_sound_path(path=""):
+    """Donne le chemin absolu des sons"""
     return os.path.abspath(
         os.path.join(get_data_path(), "sounds/", path))
 
 
 def get_config_path(path=""):
+    """Donne le chemin absolu des config"""
     return os.path.abspath(
         os.path.join(get_data_path(), "config/", path))
 
 def get_fonts_path(path=""):
+    """Donne le chemin absolu des polices"""
     return os.path.abspath(
         os.path.join(get_data_path(), "fonts/", path))
 
