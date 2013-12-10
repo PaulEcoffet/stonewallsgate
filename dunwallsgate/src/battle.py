c5be348309a3b48a2f7e76d81778e6b39e501d7a
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-12-10 12:39:51 +0100

    Addition of docstring

--- dunwallsgate/src/battle.py
+++ dunwallsgate/src/battle.py
@@ -1,3 +1,7 @@
+"""
+Ce module comporte l'objet Battle, qui décrit un combat, et les
+différentes exceptions qui peuvent se produire durant celui-ci.
+"""
 import random
 
 import inventory
@@ -6,9 +10,12 @@ from ia import IA
 
 
 class Battle(object):
-    """Represent a Battle"""
+    """Represente un Battle"""
 
     def __init__(self, team1, team2):
+        """
+        Initialise un Battle avec `team1` et `team2` des list de Characters
+        """
         self.team1 = team1
         self.team2 = team2
         self.run = [False, False]
@@ -20,10 +27,16 @@ class Battle(object):
 
     @property
     def playing_char(self):
+        """
+        Le joueur qui a la main
+        """
         return self.turn_list[self.cur_player_index]
 
     @property
     def cur_player_team(self):
+        """
+        L'indice de la team du joueur qui a la main
+        """
         if self.playing_char in self.team1:
             return 1
         else:
@@ -31,6 +44,10 @@ class Battle(object):
 
     @property
     def winner(self):
+        """
+        L'indice de la team qui a gagné le combat. Si personne n'a gagné, alors
+        winner vaut None
+        """
         if not [charact for charact in self.team1 if charact.is_alive] or self.run[1]:
             return 2
         elif not [charact for charact in self.team2 if charact.is_alive] or self.run[0]:
@@ -40,9 +57,16 @@ class Battle(object):
 
     @property
     def all_characters(self):
+        """
+        La liste de l'ensemble des protagonistes du combat
+        """
         return self.team1 + self.team2
 
     def do_attack(self, target):
+        """
+        Faire attaquer effectivement le joueur qui a la main sur `target`.
+        Peut émettre une CantAttackException ou une AlreadyPlayedException.
+        """
         if not self.has_played:
             if self.can_attack(target):
                 try:
@@ -74,11 +98,18 @@ class Battle(object):
             raise AlreadyPlayedException()
 
     def can_attack(self, target):
+        """
+        Renvoie True si le joueur qui a la main peut attaquer `target`, sinon
+        retourne False
+        """
         return ((target in self.team1 and self.playing_char in self.team2)
                 or (target in self.team2 and self.playing_char in self.team1)
                 and target.is_alive)
 
     def possible_targets_attack(self):
+        """
+        Renvoie la liste des cibles possibles en attaque pour le joueur qui a la main
+        """
         if self.cur_player_team == 1:
             return [character for character in self.team2
                     if character.is_alive]
@@ -87,6 +118,12 @@ class Battle(object):
                     if character.is_alive]
 
     def change_weapon(self, weapon, ammo):
+        """
+        Change l'arme actuel du joueur qui a la main pour l'arme `weapon` avec
+        les munitions `ammo`.
+        Peut émettre une CantChangeWeaponException ou une
+        AlreadyPlayedException.
+        """
         if not self.has_played:
             if weapon in self.playing_char.inventory.items:
                 if ammo in self.playing_char.inventory.items or not ammo:
@@ -121,12 +158,19 @@ class Battle(object):
             raise AlreadyPlayedException()
 
     def do_run(self):
+        """
+        Fait fuire l'équipe du joueur qui a la main
+        """
         if self.playing_char in self.team1:
             self.run[0] = True
         else:
             self.run[1] = True
 
     def end_turn(self):
+        """
+        Termine le tour du joueur qui a la main et donne la main au joueur
+        suivant.
+        """
         self.has_played = False
         self.cur_player_index = ((self.cur_player_index + 1)
                                  % len(self.turn_list))
@@ -137,14 +181,20 @@ class Battle(object):
 
 
 class CantAttackException(Exception):
+    """Exception lancéé si le joueur ne peut pas attaquer"""
     pass
 
 
 class CantChangeWeaponException(Exception):
+    """Exception lancée si le joueur ne peut pas changer d'arme"""
     pass
 
 
 class AlreadyPlayedException(Exception):
+    """
+    Exception lancée si le joueur a déjà joué et ne peut pas effectuer une
+    deuxième action
+    """
     pass
 
 
